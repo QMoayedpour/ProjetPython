71071497081ae3435c0c0e0a3a3ce3fec9cfd3a7
QMoayedpour <quentin.moayedpour@ensae.fr>
2023-12-14 10:18:36 +0000

    Re organisation

--- analyse des données/utils/functions.py
+++ analyse des données/utils/functions.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 import plotly.express as px
 import numpy as np
+from lifelines import KaplanMeierFitter
 
 
 def afficher_colonnes_manquantes(df, seuil=0.95):
@@ -46,3 +47,59 @@ def plot_distribution_valeurs_manquantes(df):
     plt.ylabel('Taux de valeurs manquantes (%)')
 
     plt.show()
+
+
+def data_treatment(df, filter=True):
+    """Fonction pour traiter les dataframe
+
+    Args:
+        df (Dataframe): Dataframe de données clinical trials
+        filter (bool, optional): Supprimer ou non les valeurs non pertinentes. Defaults to True.
+
+    Returns:
+        Dataframe: dataframe traité
+    """
+    df['StartDate'] = pd.to_datetime(df['StartDate'], format='%B %d, %Y', errors='coerce')
+    df['CompletionDate'] = pd.to_datetime(df['CompletionDate'], format='%B %d, %Y', errors='coerce')
+    # Créer une nouvelle colonne avec la différence en jours
+    df['TimePassed'] = (df['CompletionDate'] - df['StartDate']).dt.days
+    df['TimePassed'] = df['TimePassed'].fillna(0)
+    df['Bin']=df['TimePassed'].apply(lambda x: 1 if x>5 else 0)
+    if filter:
+        df = df[df['Bin']==1].reset_index(drop=True) #On supprime les valeurs trop petites/protocoles non terminés (ou n'ayant pas de date de fin prévue)
+        df = df[df['TimePassed']<1000].reset_index(drop=True) #On supprime aussi les valeurs trop élevés considérés comme des outliers (Seuil objectif)
+       
+    return df
+
+
+
+def plot_survie(T, df, phase='Phase 3', comparaison="ALL"):
+    """Fonction pour afficher la distribution kaplan meier de survie de l'échantillon. Cette methode n'est PAS paramétrique, on modelise simplement la probabilité 
+    instantané de sortie selon le groupe. Rien n'est controllé (il peut par exemple y avoir des biais). Cette approche permet simplement d'avoir une intuition mais 
+    ne peut pas être directement interprété.
+
+    Args:
+        T (series): serie des temps passés avant la completion date par essai clinique
+        df (dataframe): dataframe lié à la série T
+        phase (str, optional): phase à étudier. Defaults to 'Phase 3'.
+        comparaison (str, optional): phase à comparer. "ALL" par défault.
+
+    Raises:
+        ValueError: Si la phase rentré n'est pas valide
+    """
+    ax = plt.subplot(111)
+    m = (df["Phase"] == phase)
+    kmf = KaplanMeierFitter()
+    kmf.fit(durations = T[m], label = "Phase 3")
+    kmf.plot_survival_function(ax = ax)
+    if comparaison=="ALL":
+        kmf.fit(T[~m], label = "Autre")  
+    else:
+        if comparaison not in df['Phase'].unique():
+            list_poss = ", ".join(df['Phase'].dropna().unique())
+            raise ValueError(f"'{comparaison}' n'est pas une valeur valide, veuillez choisir parmis les valeurs suivantes:",list_poss)     
+        j = (df["Phase"] == comparaison)
+        kmf.fit(T[j], label = comparaison) 
+
+    kmf.plot_survival_function(ax = ax, at_risk_counts = True)
+    plt.title("Survival of different gender group")
